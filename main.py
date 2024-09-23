e5cb273d9a30743ccb725ac9878cfb2b4407bdac
Dylan Stitt <dstitt527@gmail.com>
2024-09-23 14:48:07 -0400

    final

--- main.py
+++ main.py
@@ -3,12 +3,6 @@
 # Mul. Table
 
 table = [i*j for i in range(1, 11) for j in range(1, 11)]
-
-########### NO TOUCHY ###########
 for i, num in enumerate(table):
-    if i % 10 == 0:
-        print()
-
+    if i % 10 == 0: print()
     print(num, end="\t")
-print()
-#################################
