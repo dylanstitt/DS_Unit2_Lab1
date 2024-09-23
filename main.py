7783fef0a0e0938f3158e1a6bb0de43cc1d8ef7c
Dylan Stitt <dstitt527@gmail.com>
2024-09-23 13:31:06 -0400

    final

---
+++ main.py
@@ -0,0 +1,14 @@
+# Dylan Stitt
+# Unit 2 Lab 1
+# Mul. Table
+
+table = [i*j for i in range(1, 11) for j in range(1, 11)]
+
+########### NO TOUCHY ###########
+for i, num in enumerate(table):
+    if i % 10 == 0:
+        print()
+
+    print(num, end="\t")
+print()
+#################################
